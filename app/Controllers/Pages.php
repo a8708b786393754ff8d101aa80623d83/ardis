a529413c3356faab1ec55ccebf9b6a518672eed0
a6888b <ayoubbrahim68@gmail.com>
2022-02-09 16:11:23 +0100

    smarty essaie 1

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -1,15 +1,22 @@
 <?php 
 
 namespace App\Controllers; 
+require_once 'smarty/Smarty.class.php';
 
 class Pages extends BaseController{
+    protected $smarty; 
+
+    public function __construct()
+    {
+        $this->smarty = new Smarty(); 
+    }
     public function index()
     {
-        return view('index'); 
+        $this->smarty->display("app/Views/index.tpl");  
     }
 
-    public function activiter()
-    {
-        return view("activiter");
-    }
+    // public function activiter()
+    // {
+        // return view("activiter");
+    // }
 }
