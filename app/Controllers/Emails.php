34753874b0d3d11137d877e9ded45be795c30c49
Arthur <kretz.arthur68000@gmail.com>
2022-02-28 20:42:10 +0100

    Ajout Emails controller

---
+++ app/Controllers/Emails.php
@@ -0,0 +1,27 @@
+<?php 
+namespace App\Controllers; 
+use App\Config\Email;
+class Emails extends Email{
+    public string $from;
+    public string $name; 
+    public string $to; 
+    public string $subject;
+    public string $msg;
+
+    public function construct(){
+       parent::construct(); 
+   }
+   public function send(){
+    $this->setFrom($this->$from, $this->$name);
+    $this->setTo($this->$to);
+    // $this->setCC('another@another-example.com');
+    // $this->setBCC('them@their-example.com');
+
+    $this->setSubject($this->$subject);
+    $this->setMessage($this->$msg);
+
+    $this->send();
+   }
+
+//   public 
+}
