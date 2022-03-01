aa00e2f1e0ef51f62125fe89da21c690d9f259df
a688b <ayoubbrahim68@gmail.com>
2022-03-01 22:43:05 +0100

    ajout de la fonctionnaliter recherche est du resultat

--- app/Models/SearchModel.php
+++ app/Models/SearchModel.php
@@ -4,35 +4,49 @@ use CodeIgniter\Model;
 
 class SearchModel extends Model{
     public string $element; 
+   
+    protected $table         = 'clients';
+    protected $primaryKey    = 'client_id';
+    protected $allowedFields = [ "client_nom","client_prénom",
+                                "client_adresse","client_cp" ,"client_ville" ,"client_pays" ,"client_email",
+                                "client_tel", "compte_client","resrv_client","client_avis"
+                                ];
+    
     public function __construct(string $element_search){
-        $this->element = $element_search;
         parent::__construct();
+        $this->element = $element_search;
     }
 
     public function getResultHotel(){
-        $this->db->query("SELECT hotel_nom AS nom , hotel_ville AS ville , hotel_pays AS pays, hotel_contenue AS description , hotel_image AS image
-        FROM hotels
-        WHERE hotel_nom LIKE '%".$this->element."%' 
-        OR hotel_ville LIKE '%".$this->element."%' 
-        OR hotel_pays LIKE '%".$this->element."%' 
-        ")->getResult(); 
+        return $this->db->query("SELECT hotel_nom AS nom, hotel_ville AS ville , hotel_pays AS pays, hotel_contenue AS description , hotel_image AS image
+                                FROM hotels
+                                WHERE hotel_nom LIKE '%".$this->element."%' 
+                                OR hotel_ville LIKE '%".$this->element."%' 
+                                OR hotel_pays LIKE '%".$this->element."%' 
+                                OR hotel_contenue LIKE '%".$this->element."%' 
+                                ")->getResult(); 
     }
 
     public function getResultAvis(){
-        $this->db->query("SELECT  avis_titre AS avis , avis_cont AS contenue 
-        FROM avis
-        INNER JOIN hotels 
-        ON avis.avis_hotel = hotels.hotel_id
-        WHERE avis_hotel = '%".$this->element."%'")->getResult();
+        return $this->db->query("SELECT avis_titre AS avis, avis_cont AS contenue 
+                                FROM avis
+                                INNER JOIN hotels 
+                                ON avis.avis_hotel = hotels.hotel_id
+                                WHERE  avis_hotel LIKE '%".$this->element."%'
+                                OR avis_titre LIKE '%".$this->element."%' 
+                                ")->getResult();
     }
 
-
     public function getResultActiv(){
-        $this->db->query("SELECT activ_nom AS nom , activ_tarif AS prix , activ_date AS date , activ_descri AS description
-        FROM activites
-        INNER JOIN hotels
-        ON activites.hotel_id = hotels.hotel_id
-        WHERE activ_loca LIKE '%".$this->element."%' AND YEAR(activ_date) = YEAR(CURRENT_DATE) ")->getResult();
+        return $this->db->query("SELECT activ_nom AS nom, activ_tarif AS prix , activ_date AS date , activ_descri AS description, activ_image AS image
+                                FROM activites
+                                INNER JOIN hotels
+                                ON activites.hotel_id = hotels.hotel_id
+                                WHERE activ_loca LIKE '%".$this->element."%'
+                                OR activ_nom LIKE '%".$this->element."%'
+                                OR activ_descri LIKE '%".$this->element."%'  
+                                AND YEAR(activ_date) = YEAR(CURRENT_DATE) 
+                                ")->getResult();
     }
 
 
